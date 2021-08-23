f08dd5eb76ba55d936aa1bcbcb451ec87ca75f42
SailajaRadjou <p.sailaja@hotmail.com>
2021-08-24 01:29:32 +0200

    configuration la base de donnée en installant MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://radjou:sailaja@cluster0.5c8jm.mongodb.net/piiquanteDataBase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !'); 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
- });
-app.use((req, res, next) => {
-    res.status(201) ;
-    next();
- });
+});
 
- app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
-    next();
- });
+app.use(bodyParser.json());
 
- app.use((req, res, next) => {
-    console.log('La réponse a bien été reçue !'); 
- });
+app.post('/api/sauces', (req, res, next) => {
+    console.log(req.body);
+    res.status(200).json({message: 'Object Crée !'});
+});
+
+
+app.use('/api/sauces', (req, res, next) => {
+    const sauces = [
+      {
+        _id: 'oeihfzeoi',
+        name: 'Purée de piment antillais',
+        manufacturer: 'Fabricant de la sauce',
+        description: 'La purée de piment Dame Besson est composée de : piment antillais, huile de tournesol, vinaigre, sel. Antioxydant: acide ascorbique Sans colorant, sans conservateur ! ',
+        mainPepper: 'Le Piment— le principal ingrédient épicé de la sauce',
+        imageUrl: 'https://www.tilolo.fr/img/152/79416/m/p/puree-de-piment-antillais.jpg',
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        name: 'Sauce Yassa BIO',
+        manufacturer: 'Yassa',
+        description: 'Eau, oignons, jus de citron, coriandre, persil, gingembre, ail, laurier, sel de Guérande. ',
+        mainPepper: 'Poulet — le principal ingrédient épicé de la sauce',
+        imageUrl: 'http://chef-gondo.com/site/wp-content/uploads/2018/08/Capture-d%E2%80%99e%CC%81cran-2018-05-09-a%CC%80-18.07.23.png',
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(201).json(sauces);
+});
 
 module.exports = app;
