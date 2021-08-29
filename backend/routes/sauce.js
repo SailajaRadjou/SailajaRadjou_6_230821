ce0a5998259ccc2239cde256253c54b51573830a
SailajaRadjou <p.sailaja@hotmail.com>
2021-08-29 14:11:56 +0200

    configuré le controleur

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,40 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const Sauce = require('../models/Sauce');
+const sauceControl = require('../controllers/sauce');
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-     ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Object Enregistré !'}))
-        .catch(error => res.status(400).json({ error }))
-});
+router.post('/', sauceControl.createSauce);
 
-router.get('/', (req, res, next) => {
-    Sauce.find()
-      .then( sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
-});
+router.get('/', sauceControl.getAllSauces);
 
-router.get('/:id', (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
+router.get('/:id', sauceControl.getOneSauce);
 
-router.put('/:id', (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-  .then(() => res.status(200).json({ message: 'Object Modifié ! '}))
-  .catch(error => res.status(400).json({ error }));
-});
+router.put('/:id', sauceControl.modifySauce);
 
-router.delete('/:id', (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet Supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+router.delete('/:id', sauceControl.deleteSauce);
 
 module.exports = router;
