97669bbf7dc2514c7d4343b454d5d8dccd1ef103
SailajaRadjou <p.sailaja@hotmail.com>
2021-08-29 14:53:48 +0200

    créé des utilisateurs & configuré les routes d'authentification

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://RadjouSailaja:mel-ang@cluster0.eeamk.mongodb.net/sauceDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -23,5 +24,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
